814190fdb30678737c02df82b10430e79bffba3a
Sean McBride <spmcbride1201@gmail.com>
2022-03-16 10:39:41 -0400

    chore: build and install wasm3

--- install_deb.sh
+++ install_deb.sh
@@ -11,3 +11,17 @@ sudo apt-get install wabt
 
 # Install Wasmtime
 curl https://wasmtime.dev/install.sh -sSf | bash
+
+# Install wasm3
+# The prebuilt binary triggers an invalid instruction signal
+git clone https://github.com/wasm3/wasm3.git
+pushd wasm3
+git checkout v0.5.0
+mkdir -p build
+pushd build
+cmake ..
+make -j8
+sudo mv wasm3 /usr/bin/
+popd
+popd
+rm -rf wasm3
